1ab76d54e866d010abe40a755eecff81bbe771f0
EconJoe <staudt.joe@gmail.com>
2016-09-29 12:30:33 -0400

    Add files via upload

---
+++ statabatch.bat
@@ -0,0 +1,33 @@
+
+
+b:
+
+
+cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
+"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\dates_clean.do
+
+cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
+"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\ngrams_txttodta.do
+
+cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
+"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\mesh_aggregation.do
+
+cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
+"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\medline_wos_intersection.do
+
+cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
+"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\ngrams_id.do
+
+cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
+"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\ngrams_mentions.do
+
+cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
+"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\ngrams_vintage.do
+
+cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
+"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\ngrams_top.do
+
+cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
+"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\Projects\HITS\HITS5\FieldLevel\Code_Field_HITS5\metrics_fieldlevel.do
+
+pause
