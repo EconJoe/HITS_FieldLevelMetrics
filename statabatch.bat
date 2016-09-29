03e8024c9733ab0950290e2914629537b1443366
EconJoe <staudt.joe@gmail.com>
2016-09-29 12:36:52 -0400

    Add files via upload

--- statabatch.bat
+++ statabatch.bat
@@ -4,30 +4,30 @@ b:
 
 
 cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
-"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\dates_clean.do
+"C:\Program Files\Stata14\StataMP-64" /e do  https://rawgit.com/EconJoe/medline2014-processors/master/dates_clean.do
 
 cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
-"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\ngrams_txttodta.do
+"C:\Program Files\Stata14\StataMP-64" /e do  https://rawgit.com/EconJoe/medline2014-processors/master/ngrams_txttodta.do
 
 cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
-"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\mesh_aggregation.do
+"C:\Program Files\Stata14\StataMP-64" /e do  https://rawgit.com/EconJoe/medline2014-processors/master/mesh_aggregation.do
 
 cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
-"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\medline_wos_intersection.do
+"C:\Program Files\Stata14\StataMP-64" /e do  https://rawgit.com/EconJoe/medline2014-processors/master/medline_wos_intersection.do
 
 cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
-"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\ngrams_id.do
+"C:\Program Files\Stata14\StataMP-64" /e do  https://rawgit.com/EconJoe/medline2014-processors/master/ngrams_id.do
 
 cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
-"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\ngrams_mentions.do
+"C:\Program Files\Stata14\StataMP-64" /e do  https://rawgit.com/EconJoe/medline2014-processors/master/ngrams_mentions.do
 
 cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
-"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\ngrams_vintage.do
+"C:\Program Files\Stata14\StataMP-64" /e do  https://rawgit.com/EconJoe/medline2014-processors/master/ngrams_vintage.do
 
 cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
-"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\RAWDATA\MEDLINE\2014\Processors\ngrams_top.do
+"C:\Program Files\Stata14\StataMP-64" /e do  https://rawgit.com/EconJoe/medline2014-processors/master/ngrams_top.do
 
 cd B:\Research\Projects\HITS\HITS6\FieldLevel\Log_Field_HITS6
-"C:\Program Files\Stata14\StataMP-64" /e do B:\Research\Projects\HITS\HITS5\FieldLevel\Code_Field_HITS5\metrics_fieldlevel.do
+"C:\Program Files\Stata14\StataMP-64" /e do  https://rawgit.com/EconJoe/medline2014-processors/master/metrics_fieldlevel.do
 
 pause
